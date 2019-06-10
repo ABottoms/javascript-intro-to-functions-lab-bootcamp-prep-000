8a4b605191c96e7466bef32987ca08eefabc725f
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-10 19:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 function shout (string) 
-}
+{}
  return string 
  string.toUpperCase ()
-{
+{}
 function whisper (string)
-{
+{}
  return string 
  string.toLowerCase 
-} 
-console.log shout (string) toUpperCase
-}
-console.log whisper (string) toLowerCase
-{
+{}
+console.log shout (string) 
+ return string.toUpperCase ()
+{}
+console.log whisper (string) 
+ return string.toLowerCase ()
+{} 
+function sayHiToGrandma (string) 
+console.log ("I can't hear you!")
+{}
+ return string 
+ string.toLowerCase 
+{}
+function sayHiToGrandma (string)
+console.log ("YES INDEED!") 
+{}
+ return string 
+ string.toLowerCase ()
+ if (string) returns $("YESINDEED!") toUpperCase
+{}
+function sayHiToGrandma (string)
+console.log ("I love you, too")
+{}
+if (string) returns $("Iloveyou,too") 
+ return $("Iloveyou,Grandma.")
+ 
