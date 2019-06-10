4cc80cb179076b9c81aa15d273d19c9175a440fb
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-10 18:44:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ function shout (string)
  return string 
  string.toUpperCase ()
 {
-  function whisper (string)
-}
-  return string 
-  string.toLowerCase 
-}
+function whisper (string)
+{
+ return string 
+ string.toLowerCase 
+} 
 console.log shout (string) toUpperCase
+}
+console.log whisper (string) toLowerCase
+{
